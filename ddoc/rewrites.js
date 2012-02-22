d8288987b0ff6cfbe012ae4445d8fae0e63e6d10
Caolan McMahon <caolan@caolanmcmahon.com>
2012-02-22 00:45:10 -0800

    update for use with less and bootstrap

--- ddoc/rewrites.js
+++ ddoc/rewrites.js
@@ -2,5 +2,5 @@ module.exports = [
     {from: '/', to: 'index.html'},
     {from: '/modules.js', to: 'modules.js'},
     {from: '/kanso-topbar/*', to: 'kanso-topbar/*'},
-    {from: '/static/*', to: 'static/*'}
+    {from: '/css/*', to: 'css/*'}
 ];
